e23603b956829fde878a151b11ee0affc469ce44
EdsonR93 <edson.rod.p19@gmail.com>
2021-03-09 15:47:34 -0800

    Added testing framework and a testing class, Project 0 meets the MVP

---
+++ CarDealer/src/main/java/com/ui/Test.java
@@ -0,0 +1,17 @@
+package com.ui;
+
+import com.database.DataBaseServices;
+import com.enterprise.annotations.TestClass;
+import com.enterprise.annotations.TestMethod;
+
+@TestClass
+public class Test {
+    DataBaseServices DB = DataBaseServices.getInstance();
+    public Test(){
+
+    }
+    @TestMethod
+    public String callTestMethod(){
+        return DB.getTestUsername();
+    }
+}
