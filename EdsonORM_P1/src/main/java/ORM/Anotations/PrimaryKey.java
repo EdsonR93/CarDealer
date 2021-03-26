356d460c8706bc03cfb056369bbbe31d13df28cb
EdsonR93 <edson.rod.p19@gmail.com>
2021-03-26 13:02:33 -0600

    Before presentation, fixed update class to accept objects and update based on provided ID

--- EdsonORM_P1/src/main/java/ORM/Anotations/PrimaryKey.java
+++ EdsonORM_P1/src/main/java/ORM/Anotations/PrimaryKey.java
@@ -3,7 +3,7 @@ package ORM.Anotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PrimaryKey {
     String name() default "";
 }
